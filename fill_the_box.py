3fe6ee4b70cadbbd906efb7142154a3f734c0791
Svilen Ivanov <102068545+TheShadowTM@users.noreply.github.com>
2022-06-02 13:33:13 +0300

    Exercises Tenth Problem

---
+++ fill_the_box.py
@@ -0,0 +1,17 @@
+def fill_the_box(height, width, length, *args):
+    box_size = int(height) * int(width) * int(length)
+    item_volumes = 0
+    for item in args:
+        if item == 'Finish':
+            break
+        item_volumes += int(item)
+
+    if item_volumes > box_size:
+        return f"No more free space! You have {item_volumes - box_size} more cubes."
+    else:
+        return f"There is free space in the box. You could put {box_size - item_volumes} more cubes."
+
+
+print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
+print(fill_the_box(5, 5, 2, 40, 11, 7, 3, 1, 5, "Finish"))
+print(fill_the_box(10, 10, 10, 40, "Finish", 2, 15, 30))
